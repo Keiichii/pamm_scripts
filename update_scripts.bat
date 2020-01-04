3e1de325a16f33d3875320d2c0422307f237a8ca
Artem <artem.usov@mail.com>
2020-01-04 09:43:08 +0300

    new link

--- update_scripts.bat
+++ update_scripts.bat
@@ -1,4 +1,5 @@
 cd c:\scripts\pamm_scripts
 ..\PortableGit\bin\git reset --hard
 ..\PortableGit\bin\git pull
-REM mklink c:\scripts\update_pamm.py c:\scripts\pamm_scripts\update_pamm.py
+del c:\scripts\logger.py
+mklink c:\scripts\logger.py c:\scripts\pamm_scripts\logger.py
