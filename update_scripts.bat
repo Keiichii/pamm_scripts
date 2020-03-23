baf8b9a4d870eb130eae9946d27ed23d23e87d29
Artem <artem.usov@mail.com>
2020-03-23 21:59:05 +0300

    LINK TO db_unlinked_poses

--- update_scripts.bat
+++ update_scripts.bat
@@ -2,4 +2,4 @@ cd c:\scripts\pamm_scripts
 ..\PortableGit\bin\git reset --hard
 ..\PortableGit\bin\git pull
 REM del c:\scripts\copy_test_pos.txt
-REM mklink c:\scripts\db_time_diff.py c:\scripts\pamm_scripts\db_time_diff.py
+mklink c:\scripts\db_unlinked_poses.py c:\scripts\pamm_scripts\db_unlinked_poses.py
